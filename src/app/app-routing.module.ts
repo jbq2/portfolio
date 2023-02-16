7e2589a37c794a954c139f8b6ca7cf1b48317b14
Joshua Quizon <joshquizon@gmail.com>
2023-02-16 15:24:33 -0500

    populating projects page with basic project information

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './component/home-page/home-page.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent},
   { path: 'projects', component: ProjectsPageComponent }
 ];
