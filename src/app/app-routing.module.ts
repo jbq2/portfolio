6714dd15f23e7c2abeb42fa8583b070c0f7dc3cd
Joshua Quizon <joshquizon@gmail.com>
2023-02-16 00:14:40 -0500

    creating projects page template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ProjectsPageComponent } from './component/projects-page/projects-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './component/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent},
+  { path: 'projects', component: ProjectsPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/projects-page/projects-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-projects-page',
+  templateUrl: './projects-page.component.html',
+  styleUrls: ['./projects-page.component.css']
+})
+export class ProjectsPageComponent {
+
+}
