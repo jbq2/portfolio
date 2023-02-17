04e7a9a30efac779eb7a77f239bd405b7cf95ce5
Joshua Quizon <joshquizon@gmail.com>
2023-02-16 22:11:54 -0500

    creating about me section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './component/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent },
-  { path: 'home', component: HomePageComponent},
-  { path: 'projects', component: ProjectsPageComponent }
+  { path: '', component: LandingPageComponent }
 ];
 
 @NgModule({
